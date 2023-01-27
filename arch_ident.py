868da609e9228cc02f3e8b57a82d5ce589448c97
Benjamin Bradley <29411864+notjustanyben@users.noreply.github.com>
2023-01-27 13:50:39 -0700

    Add files via upload

---
+++ arch_ident.py
@@ -0,0 +1,30 @@
+import subprocess
+
+arm_arch = "aarch64"
+arm_arch2 = "ARM64"
+intel_arch = "x86"
+
+# arch = subprocess.Popen(["uname -m"], shell=True, encoding="utf-8")
+# arch = subprocess.run(["uname -m"], shell=True, encoding="utf-8")
+
+def is_arm():
+    arch = subprocess.check_output(["uname -a"], shell=True, encoding="utf-8")
+    if str(arm_arch) or str(arm_arch2) in arch:
+        return True
+    else:
+        return False
+is_arm()
+
+def is_intel():
+    arch = subprocess.check_output(["uname -a"], shell=True, encoding="utf-8")
+    if str(intel_arch) in arch and arm_arch and arm_arch2 not in arch:
+        return True
+    else:
+        return False
+is_intel()
+
+if is_arm():
+    print("\naarch64/ARM64 Architecture found!\n")
+
+if is_intel():
+    print("\nx86/Intel Architecture found!\n")
